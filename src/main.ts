ed0dd850439350426b6af8e2797f6e5b61879985
SlimRunner <dv.polygon@gmail.com>
2026-02-20 01:45:08 -0800

    Implement fully articulated cart

--- src/main.ts
+++ src/main.ts
@@ -2,42 +2,22 @@ import { ComponentLayoutOptions, tiny } from "../tiny-graphics";
 import { defs } from "../examples/common";
 import { math } from "../tiny-graphics-math";
 import { UVShader } from "./shaders/UVShader";
+import { SolidColor } from "./shaders/solidColor";
+import { Axis3D } from "./shapes/axis3d";
+import { GimbalCamera } from "./components/gimbalCamera";
+import { SimpleGrid } from "./shapes/simpleGrid";
+import { CartArmature, CartNodeNames } from "./rigging/cartArmature";
 
 export class BumperCarsBase extends tiny.Component {
-  render_animation(context: tiny.Component): void {
-    const time = (this.uniforms.animation_time ?? 0) / 1000;
-
-    const camLoc = math
-      .vec3(Math.cos(time), Math.sin(Math.sin(time)), Math.sin(time))
-      .normalized()
-      .times(10);
-
-    tiny.Shader.assign_camera(
-      math.Mat4.look_at(camLoc, math.vec3(0, 0, 0), math.vec3(0, 1, 0)),
-      this.uniforms,
-    );
-
-    this.uniforms.projection_transform = math.Mat4.perspective(
-      Math.PI / 4,
-      context.width / context.height,
-      0.2,
-      100,
-    );
-
-    const light_position = camLoc.to4(1);
-    this.uniforms.lights = [
-      defs.Phong_Shader.light_source(
-        light_position,
-        math.color(1, 1, 1, 1),
-        1000000,
-      ),
-    ];
-  }
-}
-
-export class BumperCars extends BumperCarsBase {
   shapes: {
+    grid: SimpleGrid;
     box: defs.Cube;
+    cyl: defs.Cylindrical_Tube;
+    ball: defs.Subdivision_Sphere;
+    disc: defs.Regular_2D_Polygon;
+  };
+  drawables: {
+    axes3d: Axis3D;
   };
   colors: {
     red: math.Vector4;
@@ -53,6 +33,18 @@ export class BumperCars extends BumperCarsBase {
       specularity: number;
       color: math.Vector4;
     };
+    solid: {
+      shader: tiny.Shader;
+      color: math.Vector4;
+    };
+  };
+  armatures: {
+    cart1: CartArmature;
+    // cart2: CartArmature;
+  };
+
+  globalProps: {
+    gcam?: GimbalCamera;
   };
 
   constructor() {
@@ -60,6 +52,7 @@ export class BumperCars extends BumperCarsBase {
 
     const uvShader = new UVShader();
     const phongShader = new defs.Phong_Shader();
+    const solidColor = new SolidColor();
 
     this.materials = {
       uvSimple: {
@@ -72,17 +65,108 @@ export class BumperCars extends BumperCarsBase {
         specularity: 0.5,
         color: math.color(0.9, 0.5, 0.9, 1),
       },
+      solid: {
+        shader: solidColor,
+        color: math.vec4(0.6, 0.6, 0.6, 1),
+      },
     };
 
+    const grid = new SimpleGrid(11, 11, { x: [-5, 5], z: [-5, 5] });
+    const discShape = new defs.Regular_2D_Polygon(1, 16);
     const cubeShape = new defs.Cube();
+    const sphereShape = new defs.Subdivision_Sphere(4);
+    const closedTube = new defs.Capped_Cylinder(1, 24, [
+      [0, 2],
+      [0, 1],
+    ]);
 
     this.shapes = {
+      grid: grid,
       box: cubeShape,
+      cyl: closedTube,
+      ball: sphereShape,
+      disc: discShape,
+    };
+
+    // these are NOT tiny graphic shapes, but collections of shapes.
+    // Ideally only to be used as collections of actual shapes.
+    const axes3d = new Axis3D({
+      length: 5,
+      headRatio: {
+        height: 0.2,
+        width: 1,
+      },
+    });
+    this.drawables = {
+      axes3d,
     };
 
     this.colors = {
       red: math.color(0.8, 0.1, 0.1, 1),
     };
+
+    this.globalProps = {};
+
+    this.armatures = {
+      cart1: new CartArmature({
+        dimensions: {
+          wheelbase: 1.6,
+          axleTrack: 1.2,
+          rimSize: 0.4064,
+          tireWallSize: 0.13975,
+          tireWidth: 0.215,
+          roofHeight: 1.2,
+          armLinkLength: 1.25,
+          armLinkRadius: 0.05,
+          sawRadius: 0.3,
+        },
+        meshes: {
+          arm1: closedTube,
+          arm2: closedTube,
+          chassis: cubeShape,
+          saw: discShape,
+          wheel: closedTube,
+        },
+      }),
+    };
+  }
+
+  render_layout(div: HTMLDivElement, options?: ComponentLayoutOptions): void {
+    super.render_layout(div, options);
+    const canvas = this.canvas ?? document.getElementById("canvas")!;
+    this.globalProps.gcam = new GimbalCamera(8, canvas);
+  }
+
+  render_animation(context: tiny.Component): void {
+    // const time = (this.uniforms.animation_time ?? 0) / 1000;
+
+    // temporary camera for modeling
+    const { cameraMatrix, position } =
+      this.globalProps.gcam!.getCameraTransform();
+
+    tiny.Shader.assign_camera(cameraMatrix, this.uniforms);
+
+    this.uniforms.projection_transform = math.Mat4.perspective(
+      Math.PI / 4,
+      context.width / context.height,
+      0.2,
+      100,
+    );
+
+    const light_position = position.to4(1);
+    this.uniforms.lights = [
+      defs.Phong_Shader.light_source(
+        light_position,
+        math.color(1, 1, 1, 1),
+        1000000,
+      ),
+    ];
+  }
+}
+
+export class BumperCars extends BumperCarsBase {
+  constructor() {
+    super();
   }
 
   render_animation(context: tiny.Component): void {
@@ -103,10 +187,30 @@ export class BumperCars extends BumperCarsBase {
     );
     GL.enable(GL.DEPTH_TEST);
 
-    this.shapes.box.draw(context, this.uniforms, math.Mat4.identity(), {
-      ...this.materials.plastic,
-      color: this.colors.red,
+    // this.shapes.ball.draw(context, this.uniforms, math.Mat4.identity(), {
+    //   ...this.materials.plastic,
+    //   color: this.colors.red,
+    // });
+
+    this.armatures.cart1.armature.arcs.root.traverse((joint, node, matrix) => {
+      const name = node.name as CartNodeNames;
+      node.shape.draw(
+        context,
+        this.uniforms,
+        matrix,
+        this.materials.uvSimple,
+        // "LINE_STRIP",
+      );
+      // if (node.name === "wheelRR") {
+      // }
     });
+    this.shapes.grid.draw(
+      context,
+      this.uniforms,
+      math.Mat4.identity(),
+      this.materials.solid,
+    );
+    this.drawables.axes3d.draw(context, this.uniforms, math.Mat4.identity());
   }
 
   render_controls(): void {
