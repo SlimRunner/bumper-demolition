561a2b0953e92d3fc978362fdd86f48407d753cb
SlimRunner <dv.polygon@gmail.com>
2026-02-26 22:57:36 -0800

    Extend shape that uses OBJ parser

--- src/shapes/fileMesh.ts
+++ src/shapes/fileMesh.ts
@@ -1,6 +1,140 @@
+import { tiny, Uniforms, MaterialRecord } from "../../tiny-graphics";
+import { math } from "../../tiny-graphics-math";
 import { createError } from "../utils/error";
+import { normalizeLines } from "../utils/text";
+import { affineTransform, VectorKind } from "../utils/math";
 
 export const OBJParserError = createError("OBJParserError");
+
+export class FileMesh extends tiny.Shape {
+  private _ready = false;
+  private _transform?: math.Mat4;
+
+  constructor(filename: string, preTransform?: math.Mat4) {
+    super("position", "normal", "texture_coord");
+
+    this._transform = preTransform;
+    this.loadFile(filename);
+  }
+
+  private loadFile(filename: string) {
+    return fetch(filename)
+      .then((res) => {
+        if (res.ok) return Promise.resolve(res.text());
+        else return Promise.reject(res.status);
+      })
+      .then((objFile) => {
+        this.loadData(objFile);
+      })
+      .catch((err) => {
+        throw "OBJ error: file not found or format is unsupported.";
+      });
+  }
+
+  private loadData(objFile: string) {
+    let lineNumber = 0;
+    let errors = 0;
+    const expressions = normalizeLines(objFile);
+    const lines = expressions.split("\n");
+
+    const vertices: math.Vector3[] = [];
+    const vertNormals: math.Vector3[] = [];
+    const textures: math.Vector<2>[] = [];
+    const faces: _3tuple<FaceIndexPack>[] = [];
+
+    // parsing step: in this loop all the original values are
+    // accumulated. TypeScript ensures that you can trust every single
+    // value you find in the switch-case. If you need to implement more
+    // types of OBJ lines simply edit the switch case and add the
+    // appropriate token dispatcher below in `parseOBJLine`
+    for (const line of lines) {
+      ++lineNumber;
+      try {
+        const expr: exprPayload = parseOBJLine(line);
+
+        switch (expr.ident) {
+          case "mtllib":
+            throw new OBJParserError("not implemented");
+          case "v":
+            if (this._transform) {
+              const v = affineTransform(
+                this._transform,
+                expr.params.coords,
+                VectorKind.point,
+              );
+              vertices.push(math.vec3(...v));
+            } else {
+              vertices.push(math.vec3(...expr.params.coords));
+            }
+            break;
+          case "f":
+            faces.push(expr.params.indices);
+            break;
+          case "vt":
+            textures.push(math.Vector.create(...expr.params.coords));
+            break;
+          case "vn":
+            if (this._transform) {
+              const n = affineTransform(
+                this._transform,
+                expr.params.coords,
+                VectorKind.vector,
+              );
+              vertNormals.push(math.vec3(...n));
+            } else {
+              vertNormals.push(math.vec3(...expr.params.coords));
+            }
+            break;
+          case "usemtl":
+            throw new OBJParserError("not implemented");
+          case "s":
+            throw new OBJParserError("not implemented");
+          case "o":
+            throw new OBJParserError("not implemented");
+          case "g":
+            throw new OBJParserError("not implemented");
+          default: // just in case this code is modified in JS
+            throw new Error("Parser type safety violated");
+        }
+      } catch (error: unknown) {
+        if (error instanceof OBJParserError) {
+          ++errors;
+          console.error(`${error.name}: line ${lineNumber}: ${error.message}`);
+          continue;
+        } else {
+          throw error;
+        }
+      }
+
+      // here is where all the data is pushed into the tiny-graphics Shape
+      for (const tri of faces) {
+        for (const idx of tri) {
+          if (this._transform) {
+            this.arrays.position!.push(vertices[idx.vertex - 1]);
+            this.arrays.normal!.push(vertNormals[idx.normal - 1]);
+          } else {
+            this.arrays.position!.push(vertices[idx.vertex - 1]);
+            this.arrays.normal!.push(vertNormals[idx.normal - 1]);
+          }
+          this.arrays.texture_coord!.push(textures[idx.texture - 1]);
+        }
+      }
+      this._ready = true;
+    }
+  }
+
+  draw(
+    webgl_manager: tiny.Component,
+    uniforms: Uniforms,
+    model_transform: math.Mat4,
+    material: MaterialRecord,
+    type?: keyof WebGL2RenderingContext,
+  ): void {
+    if (this._ready) {
+      super.draw(webgl_manager, uniforms, model_transform, material, type);
+    }
+  }
+}
 
 class TokenStream {
   private i = 0;
