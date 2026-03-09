135458588aeeaf49249868fb740b30b5c251cb7f
SlimRunner <dv.polygon@gmail.com>
2026-03-09 02:24:06 -0700

    Add shaders for high density particles

---
+++ src/shapes/particleShape.ts
@@ -0,0 +1,55 @@
+import { math } from "../../tiny-graphics-math";
+import {
+  GPUAddresses,
+  tiny,
+  Uniforms,
+  MaterialRecord,
+} from "../../tiny-graphics";
+
+export class ParticleShape extends tiny.Shape {
+  max: number;
+  count: number;
+
+  constructor(max_particles: number) {
+    super("position");
+
+    this.max = max_particles;
+
+    this.arrays.position = [];
+    for (let i = 0; i < max_particles; i++)
+      this.arrays.position.push(math.vec3(0, 0, 0));
+
+    this.count = 0;
+  }
+
+  set_particles(particles: math.Vector3[]) {
+    this.count = Math.min(particles.length, this.max);
+
+    for (let i = 0; i < this.count; i++) {
+      const p = particles[i];
+      this.arrays.position![i][0] = p[0];
+      this.arrays.position![i][1] = p[1];
+      this.arrays.position![i][2] = p[2];
+    }
+  }
+
+  draw(
+    webgl_manager: tiny.Component,
+    uniforms: Uniforms,
+    model_transform: math.Mat4,
+    material: MaterialRecord,
+  ): void {
+    const GL = webgl_manager.context!;
+    this.copy_onto_graphics_card(GL, ["position"], false);
+
+    material.shader!.activate(
+      GL,
+      this.gpu_instances.get(GL)?.webGL_buffer_pointers!,
+      uniforms,
+      model_transform,
+      material,
+    );
+
+    GL.drawArrays(GL.POINTS, 0, this.count);
+  }
+}
