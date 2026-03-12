40d4f275b160471a60562973adf9fdc5b0f41223
SlimRunner <dv.polygon@gmail.com>
2026-03-12 06:32:09 -0700

    Implement rounded 3d bounding box
    Changes:
    - implement full bounding box
    - implement cached matrix transformation for each physics iteration
    - fix wrong "up" vector in update tire function

--- src/rigging/cartArmature.ts
+++ src/rigging/cartArmature.ts
@@ -431,7 +431,7 @@ export class CartArmature {
 
   updateFrontWheels(angleLeft: number, angleRight: number) {
     // negated so that < 0 means left and vice-versa
-    this.arcs.wheelHubFL.setAngle("ry", -angleLeft);
-    this.arcs.wheelHubFR.setAngle("ry", -angleRight);
+    this.arcs.wheelHubFL.setAngle("ry", angleLeft);
+    this.arcs.wheelHubFR.setAngle("ry", angleRight);
   }
 }
